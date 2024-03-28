2c2b5d558a9c58722e174ea2c776867581df50c0
Batuhan Taskaya <batuhan@python.org>
2024-03-28 14:40:28 -0700

    change the implementation

--- examples/text_to_image/train_text_to_image.py
+++ examples/text_to_image/train_text_to_image.py
@@ -578,123 +578,116 @@ def parse_args():
     return args
 
 
-from typing import Optional, Tuple, Union
-
-from transformers.models.clip.modeling_clip import (
-    BaseModelOutputWithPooling,
-    _create_4d_causal_attention_mask,
-    _prepare_4d_attention_mask,
-)
-
-SKS_TOKEN = 48136
+import torch
+from transformers import CLIPTextModel
+from typing import Any, Callable, Dict, Optional, Tuple, Union, List
+from transformers.modeling_outputs import BaseModelOutputWithPooling
+from transformers.models.clip.modeling_clip import _make_causal_mask, _expand_mask
 
 
-def text_encoder_fwd(
-    text_transformer,
-    input_ids: Optional[torch.Tensor] = None,
-    face_embeddings: Optional[torch.Tensor] = None,
-    attention_mask: Optional[torch.Tensor] = None,
-    position_ids: Optional[torch.Tensor] = None,
-    output_attentions: Optional[bool] = None,
-    output_hidden_states: Optional[bool] = None,
-    return_dict: Optional[bool] = None,
-) -> Union[Tuple, BaseModelOutputWithPooling]:
-    r"""
-    Returns:
+class CLIPTextModelWrapper(CLIPTextModel):
+    # Adapted from https://github.com/huggingface/transformers/blob/v4.34.1/src/transformers/models/clip/modeling_clip.py#L812
+    # Modified to accept precomputed token embeddings "input_token_embs" as input or calculate them from input_ids and return them.
+    def forward(
+        self,
+        input_ids: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.Tensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        input_token_embs: Optional[torch.Tensor] = None,
+        return_token_embs: Optional[bool] = False,
+    ) -> Union[Tuple, torch.Tensor, BaseModelOutputWithPooling]:
 
-    """
-    output_attentions = (
-        output_attentions
-        if output_attentions is not None
-        else text_transformer.config.output_attentions
-    )
-    output_hidden_states = (
-        output_hidden_states
-        if output_hidden_states is not None
-        else text_transformer.config.output_hidden_states
-    )
-    return_dict = (
-        return_dict
-        if return_dict is not None
-        else text_transformer.config.use_return_dict
-    )
+        if return_token_embs:
+            return self.text_model.embeddings.token_embedding(input_ids)
 
-    if input_ids is None:
-        raise ValueError("You have to specify input_ids")
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
-    input_shape = input_ids.size()
-    input_ids = input_ids.view(-1, input_shape[-1])
-    try:
-        sks_token_index = int(torch.where(input_ids == SKS_TOKEN)[1][0])
-    except IndexError:
-        raise ValueError("your prompt doesn't have sks in it!!!!")
+        output_attentions = output_attentions if output_attentions is not None else self.text_model.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.text_model.config.output_hidden_states
+        )
+        return_dict = return_dict if return_dict is not None else self.text_model.config.use_return_dict
 
-    hidden_states = text_transformer.embeddings(
-        input_ids=input_ids, position_ids=position_ids
-    )
+        if input_ids is None:
+            raise ValueError("You have to specify input_ids")
 
-    pad_left = 0
-    pad_right = 768 - 512
-    hidden_states[:, sks_token_index, :] = face_embeddings
+        input_shape = input_ids.size()
+        input_ids = input_ids.view(-1, input_shape[-1])
 
-    # CLIP's text model uses causal mask, prepare it here.
-    # https://github.com/openai/CLIP/blob/cfcffb90e69f37bf2ff1e988237a0fbe41f33c04/clip/model.py#L324
-    causal_attention_mask = _create_4d_causal_attention_mask(
-        input_shape, hidden_states.dtype, device=hidden_states.device
-    )
-    # expand attention_mask
-    if attention_mask is not None:
-        # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
-        attention_mask = _prepare_4d_attention_mask(attention_mask, hidden_states.dtype)
+        hidden_states = self.text_model.embeddings(input_ids=input_ids, position_ids=position_ids, inputs_embeds=input_token_embs)
 
-    encoder_outputs = text_transformer.encoder(
-        inputs_embeds=hidden_states,
-        attention_mask=attention_mask,
-        causal_attention_mask=causal_attention_mask,
-        output_attentions=output_attentions,
-        output_hidden_states=output_hidden_states,
-        return_dict=return_dict,
-    )
+        # CLIP's text model uses causal mask, prepare it here.
+        # https://github.com/openai/CLIP/blob/cfcffb90e69f37bf2ff1e988237a0fbe41f33c04/clip/model.py#L324
+        causal_attention_mask = _make_causal_mask(input_shape, hidden_states.dtype, device=hidden_states.device)
+        # expand attention_mask
+        if attention_mask is not None:
+            # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
+            attention_mask = _expand_mask(attention_mask, hidden_states.dtype)
 
-    last_hidden_state = encoder_outputs[0]
-    last_hidden_state = text_transformer.final_layer_norm(last_hidden_state)
+        encoder_outputs = self.text_model.encoder(
+            inputs_embeds=hidden_states,
+            attention_mask=attention_mask,
+            causal_attention_mask=causal_attention_mask,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
 
-    if text_transformer.eos_token_id == 2:
-        # The `eos_token_id` was incorrect before PR #24773: Let's keep what have been done here.
-        # A CLIP model with such `eos_token_id` in the config can't work correctly with extra new tokens added
-        # ------------------------------------------------------------
-        # text_embeds.shape = [batch_size, sequence_length, transformer.width]
-        # take features from the eot embedding (eot_token is the highest number in each sequence)
-        # casting to torch.int for onnx compatibility: argmax doesn't support int64 inputs with opset 14
-        pooled_output = last_hidden_state[
-            torch.arange(last_hidden_state.shape[0], device=last_hidden_state.device),
-            input_ids.to(dtype=torch.int, device=last_hidden_state.device).argmax(
-                dim=-1
-            ),
-        ]
-    else:
-        # The config gets updated `eos_token_id` from PR #24773 (so the use of exta new tokens is possible)
-        pooled_output = last_hidden_state[
-            torch.arange(last_hidden_state.shape[0], device=last_hidden_state.device),
-            # We need to get the first position of `eos_token_id` value (`pad_token_ids` might equal to `eos_token_id`)
-            (
-                input_ids.to(dtype=torch.int, device=last_hidden_state.device)
-                == text_transformer.eos_token_id
-            )
-            .int()
-            .argmax(dim=-1),
-        ]
+        last_hidden_state = encoder_outputs[0]
+        last_hidden_state = self.text_model.final_layer_norm(last_hidden_state)
 
-    if not return_dict:
-        return (last_hidden_state, pooled_output) + encoder_outputs[1:]
+        if self.text_model.eos_token_id == 2:
+            # The `eos_token_id` was incorrect before PR #24773: Let's keep what have been done here.
+            # A CLIP model with such `eos_token_id` in the config can't work correctly with extra new tokens added
+            # ------------------------------------------------------------
+            # text_embeds.shape = [batch_size, sequence_length, transformer.width]
+            # take features from the eot embedding (eot_token is the highest number in each sequence)
+            # casting to torch.int for onnx compatibility: argmax doesn't support int64 inputs with opset 14
+            pooled_output = last_hidden_state[
+                torch.arange(last_hidden_state.shape[0], device=last_hidden_state.device),
+                input_ids.to(dtype=torch.int, device=last_hidden_state.device).argmax(dim=-1),
+            ]
+        else:
+            # The config gets updated `eos_token_id` from PR #24773 (so the use of exta new tokens is possible)
+            pooled_output = last_hidden_state[
+                torch.arange(last_hidden_state.shape[0], device=last_hidden_state.device),
+                # We need to get the first position of `eos_token_id` value (`pad_token_ids` might equal to `eos_token_id`)
+                (input_ids.to(dtype=torch.int, device=last_hidden_state.device) == self.text_model.eos_token_id)
+                .int()
+                .argmax(dim=-1),
+            ]
 
-    return BaseModelOutputWithPooling(
-        last_hidden_state=last_hidden_state,
-        pooler_output=pooled_output,
-        hidden_states=encoder_outputs.hidden_states,
-        attentions=encoder_outputs.attentions,
-    )
+        if not return_dict:
+            return (last_hidden_state, pooled_output) + encoder_outputs[1:]
 
+        return BaseModelOutputWithPooling(
+            last_hidden_state=last_hidden_state,
+            pooler_output=pooled_output,
+            hidden_states=encoder_outputs.hidden_states,
+            attentions=encoder_outputs.attentions,
+        )
+
+import torch
+import torch.nn.functional as F
+
+def project_face_embs(input_ids, text_encoder, face_embs, arcface_token_id):
+
+    '''
+    face_embs: (N, 512) normalized ArcFace embeddings
+    '''
+
+    token_embs = text_encoder(input_ids=input_ids.repeat(len(face_embs), 1), return_token_embs=True)
+    token_embs[input_ids==arcface_token_id] = face_embs
+
+    prompt_embeds = text_encoder(
+        input_ids=input_ids,
+        input_token_embs=token_embs
+    )[0]
+
+    return prompt_embeds
 
 def main():
     args = parse_args()
@@ -793,7 +786,7 @@ def main():
     # `from_pretrained` So CLIPTextModel and AutoencoderKL will not enjoy the parameter sharding
     # across multiple gpus and only UNet2DConditionModel will get ZeRO sharded.
     with ContextManagers(deepspeed_zero_init_disabled_context_manager()):
-        text_encoder = CLIPTextModel.from_pretrained(
+        text_encoder = CLIPTextModelWrapper.from_pretrained(
             args.pretrained_model_name_or_path,
             subfolder="text_encoder",
             revision=args.revision,
@@ -814,10 +807,7 @@ def main():
 
     from insightface.app import FaceAnalysis
 
-    app = FaceAnalysis(
-        name="buffalo_l",
-        providers=["CUDAExecutionProvider", "CPUExecutionProvider"],
-    )
+    app = FaceAnalysis(name='antelopev2', providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
     app.prepare(ctx_id=0, det_size=(640, 640))
 
     # Freeze vae and set unet and TE to trainable
@@ -1164,12 +1154,13 @@ def main():
     )
     with torch.no_grad():
         input_ids = tokenizer(
-            ["a photo of sks person"] * args.train_batch_size,
+            ["photo of a id person"] * args.train_batch_size,
             max_length=tokenizer.model_max_length,
             padding="max_length",
             truncation=True,
             return_tensors="pt",
         )["input_ids"].to("cuda")
+        arcface_token_id = tokenizer.encode("id", add_special_tokens=False)[0]
 
     for epoch in range(first_epoch, args.num_train_epochs):
         train_loss = 0.0
@@ -1244,10 +1235,11 @@ def main():
                     noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
 
                 # Get the text embedding for conditioning
-                encoder_hidden_states = text_encoder_fwd(
-                    text_encoder.text_model,
+                encoder_hidden_states = project_face_embs(
                     input_ids,
-                    face_embeddings=face_embeddings,
+                    text_encoder,
+                    face_embeddings,
+                    arcface_token_id,
                 )[0]
 
                 # Get the target for loss depending on the prediction type
